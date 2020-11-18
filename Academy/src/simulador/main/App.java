e4e1f293cc96e8cb04fa589bea6bb67be2708d26
Anderson Felix <andersonfelixmsouza@gmail.com>
2020-11-18 19:08:15 -0300

    include methods toString() and equals()
    and more organization

--- Academy/src/simulador/main/App.java
+++ Academy/src/simulador/main/App.java
@@ -6,14 +6,20 @@ import simulador.code.Caixa;
 public class App {
 
   public static void main(String[] args) {
-    Robo robo = new Robo("Dersox", 9.5f, 50, 50);
+    Robo robo[] = new Robo[2];
     Caixa caixa[] = new Caixa[3];
+    
+    robo[0] = new Robo("Dersox", 9.5f, 50, 50);
+    robo[1] = new Robo("Frodox", 7.3f, 100, 100);
 
     caixa[0] = new Caixa("Bolacha Nikito", 10, 50, 45, 14.3f, 1.08f, 0.78f, 0.45f);
     caixa[1] = new Caixa("Bolacha Passatempo", 15, 55, 45, 16.5f, 1.08f, 0.80f, 0.55f);
     caixa[2] = new Caixa("Bolacha Negresco", 30, 60, 45, 21.8f, 1.20f, 0.80f, 0.65f);
 
     caixa[2].status();
+    robo[1].status();
+    System.out.println(caixa[2]);//Invoca o método toString() que é usado para imprimir detalhes do objeto.
+    System.out.println(caixa[2].equals(caixa[1]));//Invoca o método equals() que compara dois objetos
 
 //    robo.printStatus();
 //    for (int d = 0; d < 4; d++) {
