30072b1f8b98fe6320de1593ea9af3c4ad8c4fbb
Anderson Felix <andersonfelixmsouza@gmail.com>
2020-12-02 23:17:11 -0300

    I implemented a function for scheduled movements

--- Academy/src/simulador/main/App.java
+++ Academy/src/simulador/main/App.java
@@ -15,6 +15,33 @@ import simulador.code.Caixa;
 public class App {
 
   public static void main(String[] args) {
+    if (args.length != 0) {
+      args[0] = args[0].toLowerCase();
+      
+      if (args[0].equals("--author")) {
+        System.out.println("Anderson Felix Moraes de Souza");
+      } else if (args[0].equals("--version")) {
+        System.out.println("Version : 1.0.0");
+      } else if (args[0].equals("--help")) {
+        System.out.println("Exec. Movimentos agendados (EXEMPLO) :");
+        System.out.println("\t--move w a w w s d d a");
+      } else if (args[0].equals("--commands")) {
+        System.out.println("Comandos suportados :");
+        System.out.println("\t[w] : move para cima");
+        System.out.println("\t[s] : move para baixo");
+        System.out.println("\t[a] : move para esquerda");
+        System.out.println("\t[d] : move para direita");
+        System.out.println("\t[0] : sair da aplicacao");
+      } else if (args[0].equals("--move")) {
+        Robo robo = new Robo();
+        System.out.println("Movimentos agendados : ");
+        robo.movAgendados(args);
+      } else {
+        System.out.println("Argument Invalid");
+      }
+      System.exit(0);
+    }
+
     Robo robo[] = new Robo[2];
     Caixa caixa[] = new Caixa[3];
     Scanner scan = new Scanner(System.in);
@@ -34,13 +61,13 @@ public class App {
         for (int i = 0; i < 5; i++) {
           System.out.println();
         }
-          System.out.println("\n\t ___ON___ \n");
-          robo[0].printPosition();
-          System.out.println("\n");
-          tecla = scan.next().toLowerCase();
-          robo[0].setOrientacao(tecla.charAt(0));
-          
-        }catch (IllegalArgumentException ex) {
+        System.out.println("\n\t ___ON___ \n");
+        robo[0].printPosition();
+        System.out.println("\n");
+        tecla = scan.next().toLowerCase();
+        robo[0].setOrientacao(tecla.charAt(0));
+
+      } catch (IllegalArgumentException ex) {
         if (tecla.charAt(0) == '0') {
           ok = true;
         } else {
@@ -49,11 +76,10 @@ public class App {
           ok = false;
         }
       }
-      }
-      while (!ok);
-      robo[0].status();
-      caixa[0].status();
-
-    }
+    } while (!ok);
+    robo[0].status();
+    caixa[0].status();
 
   }
+
+}
